74061aac1053a1ebba859c0e1ba5ab18ea82db82
Bruno Farfán <bruno.farfan.miquel@uc.cl>
2024-09-24 18:57:56 -0300

    text_formatter.py ahora recibeargumentos desde la terminal

--- text_formatter.py
+++ text_formatter.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import csv
-
+import argparse
 
 def format_csv(data: pd.DataFrame) -> pd.DataFrame:
-    # Create a new empty dataframe with one column "Text"
+    # Create a new dataframe after dropping NaN values and sorting by 'fecha'
     new_data = data.dropna(subset=["source_glosa", "delito", "lugar", "comuna", "text"])
     new_data.sort_values(by="fecha", inplace=True)
+
+    # Create 'Text' column by concatenating relevant columns
     text_df = pd.DataFrame(columns=["Text"])
     text_df["Text"] = new_data.apply(lambda x: f"{x['fecha']} {x['source_glosa']} {x['delito']} {x['lugar']} {x['comuna']} {x['text']}", axis=1)
     text_df["Text"] = text_df["Text"].str.replace("\n", " ")
@@ -37,11 +39,19 @@ def csv2txt(data: pd.DataFrame, output_file: str) -> None:
                 previous_month = month
             f.write(f"{text}\n")
 
+if __name__ == "__main__":
+    # Setting up argument parsing
+    parser = argparse.ArgumentParser(description="Process CSV and output formatted text.")
+    parser.add_argument("input_file", help="Path to the input CSV file")
+    parser.add_argument("output_file", help="Path to the output text file")
 
-original_data = pd.read_csv("data/delitos_txt.csv")
+    args = parser.parse_args()
 
-formatted_data = format_csv(original_data)
+    # Reading the input CSV file
+    original_data = pd.read_csv(args.input_file)
 
-formatted_data.to_csv("data/formatted_text.csv", index=False, quoting=csv.QUOTE_ALL)
+    # Formatting the data
+    formatted_data = format_csv(original_data)
 
-csv2txt(formatted_data, "data/formatted_text.txt")
+    # Outputting directly to the text file
+    csv2txt(formatted_data, args.output_file)
